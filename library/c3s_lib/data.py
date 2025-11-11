6c6f20cdd1cd92c4da60541f785e2cf5a4b7bc4e
Robin Kooyman <robin.kooyman.work@gmail.com>
2025-11-11 23:48:55 +0100

    add mars

--- library/c3s_lib/data.py
+++ library/c3s_lib/data.py
@@ -1,8 +1,9 @@
+import subprocess
 import pandas as pd
 import xarray as xr
 import geopandas as gpd
 import beacon_api
-from datetime import datetime
+from datetime import datetime, timedelta
 from cdsapi import Client
 import tempfile
 import glob
@@ -559,3 +560,200 @@ class LocalFileCache():
         gdf = gpd.GeoDataFrame(df, geometry=gpd.points_from_xy(df.longitude, df.latitude), crs="EPSG:4326")
 
         return gdf
+    
+
+class MarsClient():
+    def __init__(self, key: str):
+        from ecmwfapi import ECMWFService
+        self.key = key
+        self.server = ECMWFService("mars", key=key, url='https://api.ecmwf.int/v1')
+        self.check_cdo()
+    
+    def check_cdo(self):
+        import shutil, subprocess
+        if shutil.which("cdo") is None:
+            raise EnvironmentError("❌ CDO not found. Please install it (e.g. `sudo apt install cdo`).")
+
+        try:
+            v = subprocess.run(["cdo", "-V"], capture_output=True, text=True, check=True)
+            print(f"✅ {v.stdout.splitlines()[0]}")
+        except Exception as e:
+            raise EnvironmentError(f"⚠️ CDO check failed: {e}")
+    
+    def get_date_list_past_7_days(self) -> list[str]:
+        # Get current system date
+        given_date = datetime.now()
+
+        # Generate list of 7 days before the current date
+        date_list = [(given_date - timedelta(days=i)).strftime("%Y-%m-%d") for i in range(2, 8)]
+
+        # Sort ascending (oldest to newest)
+        date_list.reverse()
+        return date_list    
+    
+    def get_temp_path(self) -> str:
+        import tempfile
+        temp_file = tempfile.NamedTemporaryFile(delete=False, suffix='.nc')
+        return temp_file.name
+    
+    def fetch_t2m_mean_operational_data(self) -> gpd.GeoDataFrame:
+        time = "00:00:00/06:00:00/12:00:00/18:00:00"
+        # Fetch the current date -7 days as a list of dates
+        print(f"Fetching t2m data for past 7 days from MARS: {self.get_date_list_past_7_days()}")
+        request = {
+            "class": "od",
+            "date": self.get_date_list_past_7_days(),
+            "expver": "1",
+            "param": self.find_param_code("t2m"), # 2m temperature
+            "grid": "0.25/0.25", # 0.25 degree grid
+            "time": time,
+            "format": format,
+            "class": "od",
+            "levtype": "sfc",
+            "stream": "oper",
+            "type": "an",
+            "target": "output",
+            "format": "netcdf",
+        }
+        temp_path = self.get_temp_path()
+        self.server.execute(request,target=temp_path)
+        
+        # USE CDO to compute daily mean
+        out_daily = temp_path.replace(".nc", "_daily.nc")
+        subprocess.run([
+            "cdo", "-O", "-r", "-f", "nc4", "-s",
+            f"-daymean", f"-shifttime,3hour", temp_path, out_daily
+        ], check=True)
+        
+        ds = xr.open_dataset(out_daily)
+        # Convert to dataframe but only keep (lon, lat, time, t2m)
+        df = ds[['longitude', 'latitude', 'time', 't2m']].to_dataframe().reset_index()
+        out_daily = gpd.GeoDataFrame(df, geometry=gpd.points_from_xy(df.longitude, df.latitude), crs="EPSG:4326")
+        return out_daily
+    
+    def fetch_t2m_min_operational_data(self) -> gpd.GeoDataFrame:
+        time = "00:00:00/12:00:00"
+        # Fetch the current date -7 days as a list of dates
+        print(f"Fetching t2m data for past 7 days from MARS: {self.get_date_list_past_7_days()}")
+        request = {
+            "class": "od",
+            "step": "6/12",
+            "date": self.get_date_list_past_7_days(),
+            "expver": "1",
+            "param": self.find_param_code("tmin"), # 2m temperature
+            "grid": "0.25/0.25", # 0.25 degree grid
+            "time": time,
+            "format": format,
+            "class": "od",
+            "levtype": "sfc",
+            "stream": "oper",
+            "type": "fc",
+            "target": "output",
+            "format": "netcdf",
+        }
+        temp_path = self.get_temp_path()
+        self.server.execute(request,target=temp_path)
+        
+        # USE CDO to compute daily min
+        out_daily = temp_path.replace(".nc", "_daily.nc")
+        subprocess.run([
+            "cdo", "-O", "-r", "-f", "nc4", "-s",
+            f"-daymin", f"-shifttime,-3hour", temp_path, out_daily
+        ], check=True)
+        
+        ds = xr.open_dataset(out_daily)
+        print(ds)
+        # Convert to dataframe but only keep (lon, lat, time, tmin)
+        df = ds[['longitude', 'latitude', 'time', 'mn2t6']].to_dataframe().reset_index()
+        out_daily = gpd.GeoDataFrame(df, geometry=gpd.points_from_xy(df.longitude, df.latitude), crs="EPSG:4326")
+        return out_daily
+    
+    def fetch_t2m_max_operational_data(self) -> gpd.GeoDataFrame:
+        time = "00:00:00/12:00:00"
+        # Fetch the current date -7 days as a list of dates
+        print(f"Fetching t2m data for past 7 days from MARS: {self.get_date_list_past_7_days()}")
+        request = {
+            "class": "od",
+            "step": "6/12",
+            "date": self.get_date_list_past_7_days(),
+            "expver": "1",
+            "param": self.find_param_code("tmax"), # 2m temperature
+            "grid": "0.25/0.25", # 0.25 degree grid
+            "time": time,
+            "format": format,
+            "class": "od",
+            "levtype": "sfc",
+            "stream": "oper",
+            "type": "fc",
+            "target": "output",
+            "format": "netcdf",
+        }
+        temp_path = self.get_temp_path()
+        self.server.execute(request,target=temp_path)
+        
+        # USE CDO to compute daily max
+        out_daily = temp_path.replace(".nc", "_daily.nc")
+        subprocess.run([
+            "cdo", "-O", "-r", "-f", "nc4", "-s",
+            f"-daymax", f"-shifttime,-3hour", temp_path, out_daily
+        ], check=True)
+        
+        ds = xr.open_dataset(out_daily)
+        # Convert to dataframe but only keep (lon, lat, time, tmax)
+        df = ds[['longitude', 'latitude', 'time', 'mx2t6']].to_dataframe().reset_index()
+        out_daily = gpd.GeoDataFrame(df, geometry=gpd.points_from_xy(df.longitude, df.latitude), crs="EPSG:4326")
+        return out_daily
+    
+    def fetch_total_precipitation_operational_data(self) -> gpd.GeoDataFrame:
+        time = "00:00:00/12:00:00"
+        # Fetch the current date -7 days as a list of dates
+        print(f"Fetching tp data for past 7 days from MARS: {self.get_date_list_past_7_days()}")
+        request = {
+            "class": "od",
+            "step": "6/12",
+            "date": self.get_date_list_past_7_days(),
+            "expver": "1",
+            "param": self.find_param_code("tp"), # total precipitation
+            "grid": "0.25/0.25", # 0.25 degree grid
+            "time": time,
+            "format": format,
+            "class": "od",
+            "levtype": "sfc",
+            "stream": "oper",
+            "type": "fc",
+            "target": "output",
+            "format": "netcdf",
+        }
+        temp_path = self.get_temp_path()
+        self.server.execute(request,target=temp_path)
+        
+        # USE CDO to compute daily sum
+        out_daily = temp_path.replace(".nc", "_daily.nc")
+        subprocess.run([
+            "cdo", "-O", "-r", "-f", "nc4", "-s",
+            f"-daysum", temp_path, out_daily
+        ], check=True)
+        
+        ds = xr.open_dataset(out_daily)
+        # Convert to dataframe but only keep (lon, lat, time, tp)
+        df = ds[['longitude', 'latitude', 'time', 'tp']].to_dataframe().reset_index()
+        out_daily = gpd.GeoDataFrame(df, geometry=gpd.points_from_xy(df.longitude, df.latitude), crs="EPSG:4326")
+        return out_daily
+    
+    
+    
+    def find_param_code(self, name: str) -> str | None:
+        """
+        Find the ECMWF MARS parameter code for a given variable name.
+        Parameters: t2m, z500, mslp, tp, tmin, tmax        
+        """
+        param_codes = {
+            "t2m": "167.128",
+            "z500": "129.128",
+            "mslp": "151.128",
+            "tp": "228.128",
+            "tmin": "122.128",
+            "tmax": "121.128",
+            # Add more mappings as needed
+        }
+        return param_codes.get(name)
